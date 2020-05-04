e2ee99731b0aea7da1db6f1c85429ae6e97e2c8e
Shimoyama Hana <nn.shimoyama0219@gmail.com>
2020-05-04 16:59:53 +0900

    wip
    Scannerクラスの追加

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("数値をを入力してください:");
+        String read  = scanner.nextLine();
+        
+
+    }
+}
