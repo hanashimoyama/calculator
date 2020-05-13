4c2ff50f446555aae9072d440dd42d300e4c5e2a
Shimoyama Hana <nn.shimoyama0219@gmail.com>
2020-05-13 15:26:09 +0900

    [add]String型をint型に直すコード追加

--- src/Main.java
+++ src/Main.java
@@ -9,20 +9,23 @@ public class Main {
             String line = sc.nextLine();
             String formula = line.replace(" ", "");
 
+            int total = 0;
+
             String operators[] = {"+", "-", "*", "/"};
             for (String search : operators) {
                 int operatorPosition = formula.indexOf(search);
                 if (operatorPosition != -1) {
-                    String operator = formula.substring(operatorPosition,operatorPosition +1);
-                    String num1 = formula.substring(operatorPosition +1);
-                    String num2 = formula.substring(0,operatorPosition);
+                    String operator = formula.substring(operatorPosition, operatorPosition + 1);
+                    String num1 = formula.substring(operatorPosition + 1);
+                    String num2 = formula.substring(0, operatorPosition);
+
+                    int value1 = Integer.parseInt(num1);
+                    int value2 = Integer.parseInt(num2);
+
                 } else {
-                    System.out.println("演算子を入力してください");
+                    continue;
                 }
-
-
             }
-
         }
 
     }
