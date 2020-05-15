1b05e04a8a6612b26c5527e24d4e7922d81d5fcd
Shimoyama Hana <nn.shimoyama0219@gmail.com>
2020-05-15 16:58:38 +0900

    [wip]test2 1+1+1のような多項式の作成

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,55 @@
-//1.四則演算の実装。(ex)1 + 1)
+//1.四則演算の実装。(ex)1 + 1 + 1)
 import java.util.Scanner;
 
 public class Main {
     public static <string> void main(String[] args) {
+        int value1 = 0;
+        int value2 = 0;
+        int total = 0;
+
         Scanner sc = new Scanner(System.in);
         System.out.print("式:");
         out:
         while (sc.hasNextLine()) {
-            String line = sc.nextLine();
+            String line  = sc.nextLine();
             String formula = line.replace(" ", "");
 
-            int total = 0;
-
-            String operators[] = {"+", "-", "*", "/"};
+            String operators[] = {"+", "-", "*", "/"}; //数値の取得
             for (String search : operators) {
                 int operatorPosition = formula.indexOf(search);
                 if (operatorPosition != -1) {
                     String operator = formula.substring(operatorPosition, operatorPosition + 1);
                     String num1 = formula.substring(0, operatorPosition);
                     String num2 = formula.substring(operatorPosition + 1);
-
-
-                    int value1 = Integer.parseInt(num1);
-                    int value2 = Integer.parseInt(num2);
-
+                    value1 = Integer.parseInt(num1);
+                    value2 = Integer.parseInt(num2);
                     switch (operator) {
 
                         case "+":
-                            total = value1 + value2 ;
+                            total = value1 + value2;
                             break;
 
                         case "-":
-                            total = value1 - value2 ;
+                            total = value1 - value2;
                             break;
 
                         case "*":
-                            total = value1 * value2 ;
+                            total = value1 * value2;
                             break;
 
                         case "/":
-                            total = value1 / value2 ;
+                            total = value1 / value2;
                             break;
                     }
+
+
                 } else {
                     continue;
                 }
-                System.out.println(total);
             }
         }
-
     }
 }
+
+
+
