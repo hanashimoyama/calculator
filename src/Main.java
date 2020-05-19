fb310435d3e9a94a66564a95652308949f21a4c6
Shimoyama Hana <nn.shimoyama0219@gmail.com>
2020-05-19 15:55:07 +0900

    [add]演算子があるかチェックして見つからなかった時の処理をするコード
    その他、数式が間違っていた時にエラーを表示するコードの追加

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 //1.四則演算の実装。(ex)1 + 1 + 1)
 import java.util.Scanner;
+import java.util.Spliterator;
 
 public class Main {
     public static <string> void main(String[] args) {
@@ -11,13 +12,18 @@ public class Main {
             String formula = line.replace(" ", "");
             int total = 0;
 
-            int index1 = operatorsIndexOf(formula,0);//演算子１
-            total = Integer.parseInt(formula.substring(0,index1));//合計をいれる値を作成
-            int index2 = operatorsIndexOf(formula,index1+1);//演算子２　index1+1を入れることで1つ目の演算子よりも後ろで検索する
+            int index1 = operatorsIndexOf(formula, 0);//演算子１
+            if(index1 == -1){
+               System.out.println("正しい式を入力してください");
+               continue;
+            }
 
-            while (index2 !=-1){
-                int value2 = Integer.parseInt(formula.substring(index1+1,index2)) ; //演算子があるか検索して見つかった時の処理
-                switch (formula.charAt(index1)){
+            total = Integer.parseInt(formula.substring(0, index1));//合計をいれる値を作成
+            int index2 = operatorsIndexOf(formula, index1 + 1);//演算子２　index1+1を入れることで1つ目の演算子よりも後ろで検索する
+
+            while (index2 != -1) {
+                int value2 = Integer.parseInt(formula.substring(index1 + 1, index2)); //演算子があるか検索して見つかった時の処理
+                switch (formula.charAt(index1)) {
                     case '+':
                         total = total + value2;
                         break;
@@ -35,68 +41,48 @@ public class Main {
                         break;
                 }
                 index1 = index2;
-                index2 = operatorsIndexOf(formula,index2+1);
+                index2 = operatorsIndexOf(formula, index2 + 1);
             }
+                int value2 = Integer.parseInt(formula.substring(index1+1));
+                switch (formula.charAt(index1)){
 
+                    case '+':
+                        total = total + value2;
+                        break;
 
-            /*String operators[] = {"+", "-", "*", "/"}; //数値の取得
-            for (String search : operators) {
-                int operatorPosition = formula.indexOf(search);
-                if (operatorPosition != -1) {
-                    String operator = formula.substring(operatorPosition, operatorPosition + 1);
-                    String num1 = formula.substring(0, operatorPosition);
-                    String num2 = formula.substring(operatorPosition + 1);
-                    value1 = Integer.parseInt(num1);
-                    value2 = Integer.parseInt(num2);
-                    switch (operator) {
+                    case '-':
+                        total = total - value2;
+                        break;
 
-                        case "+":
-                            total = value1 + value2;
-                            break;
+                    case '*':
+                        total = total * value2;
+                        break;
 
-                        case "-":
-                            total = value1 - value2;
-                            break;
-
-                        case "*":
-                            total = value1 * value2;
-                            break;
-
-                        case "/":
-                            total = value1 / value2;
-                            break;
-                    }
-
-
-                } else {
-                    continue;
+                    case '/':
+                        total = total / value2;
                 }
-            }*/
+                System.out.println(total);
         }
     }
-    public static int operatorsIndexOf(String formula,int start){
-        int operatorPlusIndex = formula.indexOf("+",start); //文字列にそれぞれの演算子があるかチェック
-        int operatorMinusIndex = formula.indexOf("-",start);
-        int operatorMultiplicationIndex = formula.indexOf("*",start);
-        int operatorDivisionIndex = formula.indexOf("/",start);
+
+    public static int operatorsIndexOf(String formula, int start) {
+        int operatorPlusIndex = formula.indexOf("+", start); //文字列にそれぞれの演算子があるかチェック
+        int operatorMinusIndex = formula.indexOf("-", start);
+        int operatorMultiplicationIndex = formula.indexOf("*", start);
+        int operatorDivisionIndex = formula.indexOf("/", start);
 
         int index = operatorPlusIndex;//＋を基準にして一番近い演算子を検索してくる
 
-        if(index==-1||(index>operatorMinusIndex && operatorMinusIndex != -1)){
-            index = operatorMinusIndex;
+        if (index == -1 || (index > operatorMinusIndex && operatorMinusIndex != -1)) { // 1+2+3だとして　indexが-1だと -1>2&&2!=1でforceになっちゃう
+            index = operatorMinusIndex;                                        //なので indexが-1の時も条件が通るようにする
         }
-        if(index==-1||(index>operatorMultiplicationIndex &&operatorMultiplicationIndex != -1)){
+        if (index == -1 || (index > operatorMultiplicationIndex && operatorMultiplicationIndex != -1)) {
             index = operatorMultiplicationIndex;
         }
 
-        if (index==-1||(index>operatorDivisionIndex && operatorDivisionIndex != -1)){
+        if (index == -1 || (index > operatorDivisionIndex && operatorDivisionIndex != -1)) {
             index = operatorDivisionIndex;
         }
-        return  index;
+        return index;
     }
 }
-
-
-
-
-
