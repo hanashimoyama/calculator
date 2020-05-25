1872909642804a22945977c41165682a3289bc1c
Shimoyama Hana <nn.shimoyama0219@gmail.com>
2020-05-25 16:13:31 +0900

    [add]かけ算と割り算の演算子を探してくるメソッド作成

--- src/Main.java
+++ src/Main.java
@@ -14,34 +14,35 @@ public class Main {
             if (formula.equals("end")) {
                 System.exit(0);
             }
-            //String operators = "[+-*/]";
+
             int total = 0;
-            int index1 = operatorsIndexOf(formula, 0);//演算子１
-            if (index1 == -1) {
+            int index= operatorsIndexOf(formula);//演算子1
+            if (index == -1) {
                 System.out.println("正しい式を入力してください");
                 System.out.print("式:");
                 continue;
             }
 
-            total = Integer.parseInt(formula.substring(0, index1));//合計をいれる値を作成
-            //int priorityIndex = priorityOperators(formula,0);
-            int index2 = operatorsIndexOf(formula, index1 + 1);//演算子２　index1+1を入れることで1つ目の演算子よりも後ろで検索する
 
-            /*while (index2 != -1&&index1 !=1){
+            //to/合計をいれる値を作成tal = Integer.parseInt(formula.substring(0, index1));/
+
+
+            /*while (index1 !=1&&index2 != -1){
                 int value2 = Integer.parseInt(formula.substring(index1 + 1,index2));
-                int priorityIndex = formula.lastIndexOf(operators,index1);
+                int priorityIndex1 = formula.lastIndexOf(operators,index1);
                 int priorityValue = Integer.parseInt(formula.substring(index1-1,priorityIndex));
                 String operator1 = formula.substring(index1,index1+1);
                 String operator2 = formula.substring(index2,index2+1);
-                System.out.println("a");
-
                 switch (formula.charAt(index1)){
                     case'+':
+                        if(operator1=="+"){
+
+                    }
                 }
                 break;
             }*/
 
-            while (index2 != -1) {
+            /*while (index2 != -1) {
                 int value2 = Integer.parseInt(formula.substring(index1 + 1, index2)); //演算子がまだあるかチェックして見つかった時のループ処理
                 switch (formula.charAt(index1)) {
                     case '+':
@@ -64,25 +65,25 @@ public class Main {
                 index2 = operatorsIndexOf(formula, index2 + 1);
             }
             //ループ処理をぬけて演算子がこれ以上無いときの処理
-            int value2 = Integer.parseInt(formula.substring(index1+1));
-                switch (formula.charAt(index1)){
-                    case '+':
-                        total = total + value2;
-                        break;
+            int value2 = Integer.parseInt(formula.substring(index1 + 1));
+            switch (formula.charAt(index1)) {
+                case '+':
+                    total = total + value2;
+                    break;
 
-                    case '-':
-                        total = total - value2;
-                        break;
+                case '-':
+                    total = total - value2;
+                    break;
 
-                    case '*':
-                        total = total * value2;
-                        break;
+                case '*':
+                    total = total * value2;
+                    break;
 
-                    case '/':
-                        total = total / value2;
-                        break;
+                case '/':
+                    total = total / value2;
+                    break;
 
-                }
+            }
             System.out.println(total);
             System.out.print("式:");
                /*System.out.println("もう1度計算しますか？YESなら１を、NOなら２を押してください");
@@ -102,17 +103,18 @@ public class Main {
 
                     }
                 }*/
+        }
 
 
         }
-    }
 
 
-    public static int operatorsIndexOf(String formula, int start) {
-        int operatorPlusIndex = formula.indexOf("+", start); //文字列にそれぞれの演算子があるかチェック
-        int operatorMinusIndex = formula.indexOf("-", start);
-        int operatorMultiplicationIndex = formula.indexOf("*", start);
-        int operatorDivisionIndex = formula.indexOf("/", start);
+
+    public static int operatorsIndexOf(String formula) {
+        int operatorPlusIndex = formula.indexOf("+"); //文字列にそれぞれの演算子があるかチェック
+        int operatorMinusIndex = formula.indexOf("-");
+        int operatorMultiplicationIndex = formula.indexOf("*");
+        int operatorDivisionIndex = formula.indexOf("/");
 
         int index = operatorPlusIndex;//＋を基準にして一番近い演算子を検索してくる
 
@@ -127,17 +129,19 @@ public class Main {
             index = operatorDivisionIndex;
         }
         return index;
-
     }
+    public static int firstOperatorsIndexOf(String formula,int start){
+        int operatorMultiplicationIndex = formula.indexOf("*",start);
+        int operatorDivisionIndex = formula.indexOf("/",start);
 
-    /*public static int priorityOperators(String formula, int priority) {
-        int operatorMultiplicationIndex = formula.indexOf("*", priority);
-        int operatorDivisionIndex = formula.indexOf("/", priority);
-        int priorityIndex= operatorMultiplicationIndex;
-
-        if (priorityIndex == -1 || (priority > operatorDivisionIndex && operatorDivisionIndex != -1)) {
-            priorityIndex = operatorDivisionIndex;
+        int firstIndex = operatorMultiplicationIndex;
+        if(firstIndex == -1||operatorMultiplicationIndex>operatorDivisionIndex && operatorDivisionIndex !=-1){
+            firstIndex = operatorDivisionIndex;
         }
-        return  priorityIndex;
-    }*/
+        return firstIndex;
+    }
 }
+
+
+
+
